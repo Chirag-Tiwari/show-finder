227bc19b6d56cea80161aa29463cf5efb155b531
Chirag Tiwari <tiwarict388@gmail.com>
2022-07-21 11:13:55 +0530

    used LinkWithQuery instead of Link tag

--- src/components/LinkWithQuery.tsx
+++ src/components/LinkWithQuery.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-export const LinkWithQuery: FC<any> = ({ Children, to, ...props }) => {
+export const LinkWithQuery: FC<any> = ({ children, to, ...props }) => {
   const { search } = useLocation();
 
   return (
     <Link to={to + search} {...props}>
-      {Children}
+      {children}
     </Link>
   );
 };
